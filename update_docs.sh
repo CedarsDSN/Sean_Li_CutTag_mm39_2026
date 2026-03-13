8e7c384ca271cf924b13a9c8b417ae76e8b04fa4
Wang <wangyiz@esplhpccompbio-lv03.cm.cluster>
2026-03-12 21:37:57 -0700

    Update project documentation

--- update_docs.sh
+++ update_docs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -eo pipefail
 
 REPO=/common/lix5lab/Li_Xue_Cut_Tag_06162022/Analysis_WYZ/mm39/github/Sean_Li_CutTag_mm39_2026
 
@@ -7,9 +7,10 @@ cd "$REPO/site_src"
 make html
 
 cd "$REPO"
+mkdir -p docs
 rsync -av --delete site_src/build/html/ docs/
 touch docs/.nojekyll
 
 git add -A
-git commit -m "Update project documentation" || true
+git commit -m "Update project documentation" || echo "No changes to commit"
 git push
